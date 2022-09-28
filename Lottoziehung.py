121ac861f0ea372b00a47c7e7137be45567b8fa4
michael-zechner <michaelzechner04@gmail.com>
2022-09-28 12:05:31 +0200

    FIX

--- Lottoziehung.py
+++ Lottoziehung.py
@@ -1,29 +1,29 @@
 import random
+from select import select
 
-def init(x):
+def init(start,stop):
     selection = []
-    for i in range(1,x+1):
+    for i in range(start,stop+1):
         selection.append(i)
         dict[i] = 0
     return selection
 
 dict = {}
-selection = init(45)
+selection = []
 
 def statistik(value):
     dict[value+1] += 1
 
-def randomizer():
+def randomizer(cnt):
     i = 1
-    while i < 7:
+    while i <= cnt:
         value = random.randint(0,len(selection)-i)
         selection[value], selection[len(selection)-i] = selection[len(selection)-i], selection[value]
         statistik(value)
         i = i + 1
 
 if __name__ == "__main__":
-    cnt = 1
-    while cnt < 1000:
-        randomizer()
-        cnt = cnt + 1
+    selection = init(1,45)
+    for i in range(1000):
+        randomizer(6)
     print(dict)
